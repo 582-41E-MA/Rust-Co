eb3ae264b743ac6b04451bdc31d459cdf2862b04
Plouffe <e2395295@cmaisonneuve.qc.ca>
2024-04-05 09:08:44 -0400

    Créer fichier utilisateurs.js
    Ce fichier contiendra toute les fonctions des
    utilisateurs. Il a actuellement juste la fonction
    d'initialisation de la base de données.

---
+++ api-rustco/routes/utilisateurs.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../config/db.js");
+const { check, validationResult } = require("express-validator");
+
+
+router.get("/initialize", async (req, res) => {
+    const donneesTest = require("../data/mockUtilisateurs.js");
+    donneesTest.forEach(async (utilisateur) => {
+        console.log(utilisateur)
+        await db.collection("utilisateur").add(utilisateur);
+    });
+
+   res.statusCode = 200;
+    res.json({ message: "Données initialisées" });
+});
+
+
+module.exports = router;
