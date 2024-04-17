0554982b6b7854213062d72a33c8d20d87564827
Plouffe <e2395295@cmaisonneuve.qc.ca>
2024-04-17 15:13:25 -0400

    Enlever une ligne de code dans utilisateurs.js
    J'ai enlever une ligne de code qui allait chercher
    auth.js, sauf que auth.js n'éxiste plus alors le
    Render plantait

--- api-rustco/routes/utilisateurs.js
+++ api-rustco/routes/utilisateurs.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const db = require("../config/db.js");
 const bcrypt = require("bcrypt");
 const jwt = require('jsonwebtoken');
-const auth = require("../middlewares/auth.js");
 const { check, validationResult } = require("express-validator");
 
 
