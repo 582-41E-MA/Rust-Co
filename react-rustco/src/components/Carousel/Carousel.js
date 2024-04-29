548321aabf4dff4f0ede02c489d8f81662eba838
TurtleTipper78 <fabien.turgeon@gmail.com>
2024-04-29 12:04:55 -0400

    Carousel btn Done

--- react-rustco/src/components/Carousel/Carousel.js
+++ react-rustco/src/components/Carousel/Carousel.js
@@ -1,6 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import './Carousel.css';
 
+const FlecheDroite = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth="1.5" width="48" height="48">
+    <circle cx="12" cy="12" r="10.5" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+    <line x1="6.27" y1="12" x2="17.73" y2="12" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+    <polyline points="12.96 7.23 17.73 12 12.96 16.77" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+  </svg>
+);
+
+const FlecheGauche = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth="1.5" width="48" height="48">
+    <circle cx="12" cy="12" r="10.5" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+    <line x1="17.73" y1="12" x2="6.27" y2="12" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+    <polyline points="11.04 16.77 6.27 12 11.04 7.23" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+  </svg>
+);
+
 function CarouselC() {
   const [slide, setSlide] = useState(0);
   const images = ['/img/cover1.png', '/img/cover2.png', '/img/cover3.png'];
@@ -31,11 +47,11 @@ function CarouselC() {
             </div>
           ))}
         </div>
-        <button onClick={prevSlide} className="carousel-button carousel-button-prev custom-shadow_2 bg-black_1 opacity-90 ">
-          Prev
+        <button onClick={prevSlide} className="carousel-button carousel-button-prev">
+          <FlecheGauche />
         </button>
-        <button onClick={nextSlide} className="carousel-button carousel-button-next custom-shadow_2 bg-black_1 opacity-90 ">
-          Next
+        <button onClick={nextSlide} className="carousel-button carousel-button-next">
+          <FlecheDroite />
         </button>
       </div>
     </div>
