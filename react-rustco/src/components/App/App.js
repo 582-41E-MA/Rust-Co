01a6215706daff244e76fa2e903ed8bb400135e2
TurtleTipper78 <fabien.turgeon@gmail.com>
2024-04-29 15:29:27 -0400

    Accueil Progress

--- react-rustco/src/components/App/App.js
+++ react-rustco/src/components/App/App.js
@@ -257,7 +257,7 @@ const [check, setCheck] = useState();
    
       <div className="background flex flex-col min-h-screen">
         <Entete handleLogout={logout} logging={logging} />
-        <main className="flex-grow min-h-screen main max-w-6xl mx-auto p-4"> 
+        <main className="flex-grow min-h-screen main max-w-6xl mx-auto"> 
   
           <Router>
             <Routes >
