c9be84211e00af930d8c3c85312510a126023cb3
TurtleTipper78 <fabien.turgeon@gmail.com>
2024-05-02 17:34:00 -0400

    Traduction Login

--- react-rustco/src/components/Login/Login.js
+++ react-rustco/src/components/Login/Login.js
@@ -20,15 +20,15 @@ function Login(props) {
     <div className="block">
       <div className="flex flex-col items-center">
         <div className="rounded-2xl overflow-hidden p-6 custom-shadow bg-sand_2">
-          <h1 className="text-xl font-bold">LOGIN</h1>
+          <h1 className="text-xl font-bold">{t('connexion_menu')}</h1>
           <form onSubmit={props.handleLogin} className="form-login">
             <input type="text" name="courriel" placeholder="Usager"></input>
             <input type="password" name="mdp" placeholder="mot de passe"></input>
-            <button className="custom-button">Connexion</button>
+            <button className="custom-button">{t('connexion')}</button>
           </form>
         </div>
         <p className="align-center mt-8">
-          Vous n'avez pas encore de compte? Créez-en un <a className="text-blue-500 font-bold text-xl" href="/create-user">ICI</a>.
+        {t('login_page')} <a className="text-blue-500 font-bold text-xl" href="/create-user">{t('ici')}</a>.
         </p>
       </div>
     </div>
